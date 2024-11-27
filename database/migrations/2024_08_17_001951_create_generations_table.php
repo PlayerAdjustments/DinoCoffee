335d7da18c268c044088bc7cea906f37c81b3b33
GaelCadena5 <gaelcamada@gmail.com>
2024-11-27 13:19:30 -0600

    duplicidad, metodo en migration

--- database/migrations/2024_08_17_001951_create_generations_table.php
+++ database/migrations/2024_08_17_001951_create_generations_table.php
@@ -4,28 +4,21 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
+class CreateMidtermsTable extends Migration
 {
     /**
      * Run the migrations.
      */
     public function up(): void
     {
-        Schema::create('generations', function (Blueprint $table) {
+        Schema::create('midterms', function (Blueprint $table) {
             $table->id();
-            $table->string('code', 9)->unique();
-            $table->date('start_date');
-            $table->date('end_date');
-            $table->string('created_by', 25)->nullable();
-            $table->string('updated_by', 25)->nullable();
-
-            /**
-             * Foreign keys, timestamps and softdeletes.
-             */
-            $table->foreign('created_by')->references('matricula')->on('users')->cascadeOnUpdate()->cascadeOnDelete();
-            $table->foreign('updated_by')->references('matricula')->on('users')->cascadeOnUpdate()->cascadeOnDelete();
-            $table->timestamps();
-            $table->softDeletes();
+            $table->string('midtermCode', 30)->unique();
+            $table->string('abbreviation', 5)->unique();
+            $table->string('fullName', 75)->index();
+            $table->date('startDate')->index();
+            $table->date('endDate')->index();
+            $this->addAuditColumns($table);  // Llamar al método privado
         });
     }
 
@@ -34,6 +27,20 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('generations');
+        Schema::dropIfExists('midterms');
     }
-};
+
+    /**
+     * Método para agregar las columnas de auditoría comunes.
+     */
+    private function addAuditColumns(Blueprint $table): void
+    {
+        $table->string('created_by', 25)->nullable();
+        $table->string('updated_by', 25)->nullable();
+        $table->foreign('created_by')->references('matricula')->on('users')->cascadeOnUpdate()->cascadeOnDelete();
+        $table->foreign('updated_by')->references('matricula')->on('users')->cascadeOnUpdate()->cascadeOnDelete();
+        $table->timestamps();
+        $table->softDeletes();
+    }
+}
+
