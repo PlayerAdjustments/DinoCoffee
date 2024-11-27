5bbaa33965a7e7e9ada88bb75089a7e274a04659
GaelCadena5 <gaelcamada@gmail.com>
2024-11-27 13:22:08 -0600

    metodo en migration,ahora si el bueno

--- database/migrations/2024_08_17_001951_create_generations_table.php
+++ database/migrations/2024_08_17_001951_create_generations_table.php
@@ -4,21 +4,28 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateMidtermsTable extends Migration
+return new class extends Migration
 {
     /**
      * Run the migrations.
      */
     public function up(): void
     {
-        Schema::create('midterms', function (Blueprint $table) {
+        Schema::create('generations', function (Blueprint $table) {
             $table->id();
-            $table->string('midtermCode', 30)->unique();
-            $table->string('abbreviation', 5)->unique();
-            $table->string('fullName', 75)->index();
-            $table->date('startDate')->index();
-            $table->date('endDate')->index();
-            $this->addAuditColumns($table);  // Llamar al método privado
+            $table->string('code', 9)->unique();
+            $table->date('start_date');
+            $table->date('end_date');
+            $table->string('created_by', 25)->nullable();
+            $table->string('updated_by', 25)->nullable();
+
+            /**
+             * Foreign keys, timestamps and softdeletes.
+             */
+            $table->foreign('created_by')->references('matricula')->on('users')->cascadeOnUpdate()->cascadeOnDelete();
+            $table->foreign('updated_by')->references('matricula')->on('users')->cascadeOnUpdate()->cascadeOnDelete();
+            $table->timestamps();
+            $table->softDeletes();
         });
     }
 
@@ -27,20 +34,6 @@ class CreateMidtermsTable extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('midterms');
+        Schema::dropIfExists('generations');
     }
-
-    /**
-     * Método para agregar las columnas de auditoría comunes.
-     */
-    private function addAuditColumns(Blueprint $table): void
-    {
-        $table->string('created_by', 25)->nullable();
-        $table->string('updated_by', 25)->nullable();
-        $table->foreign('created_by')->references('matricula')->on('users')->cascadeOnUpdate()->cascadeOnDelete();
-        $table->foreign('updated_by')->references('matricula')->on('users')->cascadeOnUpdate()->cascadeOnDelete();
-        $table->timestamps();
-        $table->softDeletes();
-    }
-}
-
+};
--- database/migrations/2024_11_25_175305_create__midterms_table.php
+++ database/migrations/2024_11_25_175305_create__midterms_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
+class CreateMidtermsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -18,14 +18,7 @@ return new class extends Migration
             $table->string('fullName', 75)->index();
             $table->date('startDate')->index();
             $table->date('endDate')->index();
-            $table->string('created_by', 25)->nullable();
-            $table->string('updated_by', 25)->nullable();
-
-            $table->foreign('created_by')->references('matricula')->on('users')->cascadeOnUpdate()->cascadeOnDelete();
-            $table->foreign('updated_by')->references('matricula')->on('users')->cascadeOnUpdate()->cascadeOnDelete();
-            $table->timestamps();
-            $table->softDeletes();
-
+            $this->addAuditColumns($table);  // Llamar al método privado
         });
     }
 
@@ -36,4 +29,18 @@ return new class extends Migration
     {
         Schema::dropIfExists('midterms');
     }
-};
+
+    /**
+     * Método para agregar las columnas de auditoría comunes.
+     */
+    private function addAuditColumns(Blueprint $table): void
+    {
+        $table->string('created_by', 25)->nullable();
+        $table->string('updated_by', 25)->nullable();
+        $table->foreign('created_by')->references('matricula')->on('users')->cascadeOnUpdate()->cascadeOnDelete();
+        $table->foreign('updated_by')->references('matricula')->on('users')->cascadeOnUpdate()->cascadeOnDelete();
+        $table->timestamps();
+        $table->softDeletes();
+    }
+}
+
