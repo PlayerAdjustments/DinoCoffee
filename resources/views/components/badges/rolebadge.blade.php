20d215d6b8c7c14637bdd723af97c78f23fdf769
PlayerAdjustments <ferzg2004@gmail.com>
2025-01-08 22:29:23 -0600

    Fresh start

--- resources/views/components/badges/rolebadge.blade.php
+++
@@ -1,3 +0,0 @@
-<span class="text-xs font-medium px-2 py-0.5 rounded {{$class}}">
-    {{$role}}
-</span>
--- resources/views/components/cards/card-template.blade.php
+++
@@ -1,4 +0,0 @@
-<div
-    class="w-full p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-8 dark:bg-gray-800 dark:border-gray-700">
-    {{$slot}}
-</div>
